faf0bcf49923e4825d0f5010458b8734748798a7
Tao <tao@huangtao.me>
2011-11-05 20:01:18 +0800

    admin, add reset table

--- admin.php
+++ admin.php
@@ -2,20 +2,27 @@
     <body>
         <form action='admin.php' method='post'>
             <input type='text' name='passphrase' /><br />
-            <input type='submit' name='show' />
-            <input type='submit' name='reset' />
+            <input type='submit' name='do' value='show' />
+            <input type='submit' name='do' value='reset' />
         </form>
 <?php
 if($_POST['passphrase'] == 'top-down') {
     include './config.php';
     include './database.php';
 
-    $query = "select * from `judges` order by `id`";
-    $result = $db->query($query);
-    while($row = $result->fetch_assoc()) {
-        echo $row['id'] . ": " . $row['passwd'] . "<br />";
+    echo '<hr />';
+    if($_POST['do'] == 'reset') {
+        $db->query("truncate `votes`");
+        echo "<p>successifully trancated table 'votes'.</p>";
+    } elseif($_POST['do'] == 'show') {
+        echo "<table><thead><tr><td>#</td><td>passwd</td></tr></thead><tbody>";
+        $query = "select * from `judges` order by `id`";
+        $result = $db->query($query);
+        while($row = $result->fetch_assoc())
+            echo "<tr><td>{$row['id']}</td><td>{$row['passwd']}</td></tr>";
+        $result->free();
+        echo "</tbody></table>";
     }
-    $result->free();
 }
 ?>
     </body>

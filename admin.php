6258d00bc768bb25a9a927dbef790d40758889fc
Tao <tao@huangtao.me>
2011-11-06 02:49:34 +0800

    admin, show votes

--- admin.php
+++ admin.php
@@ -3,6 +3,7 @@
         <form action='admin.php' method='post'>
             <input type='password' name='passphrase' /><br />
             <input type='submit' name='do' value='show' />
+            <input type='submit' name='do' value='votes' />
             <input type='submit' name='do' value='reset' />
         </form>
 <?php
@@ -14,6 +15,15 @@ if($_POST['passphrase'] == 'top-down') {
     if($_POST['do'] == 'reset') {
         $db->query("truncate `votes`");
         echo "<p>successifully trancated table 'votes'.</p>";
+    } elseif($_POST['do'] == 'votes') {
+        echo "<table><thead><tr><td>judge</td><td>team</td><td>score</td></tr></thead><tbody>";
+        $query = "select * from `votes` order by `judge_id`";
+        $result = $db->query($query);
+        while($row = $result->fetch_assoc())
+            echo "<tr><td>{$row['team_id']}</td><td>{$row['judge_id']}</td><td>{$row['score']}</td></tr>";
+        $result->free();
+        echo "</tbody></table>";
+
     } elseif($_POST['do'] == 'show') {
         echo "<table><thead><tr><td>#</td><td>passwd</td></tr></thead><tbody>";
         $query = "select * from `judges` order by `id`";

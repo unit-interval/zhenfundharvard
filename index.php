830eae72cf06ba4be92fc8397fba29a1479106c1
Tao <tao@huangtao.me>
2011-11-05 15:20:32 +0800

    index.php, login page

--- index.php
+++ index.php
@@ -1,1 +1,76 @@
-<?php echo "Hello world!"; ?>
+<?php
+
+/** turn on output buffering */
+//ob_start();
+
+/** prepare session cookie */
+session_name(SESSNAME);
+session_start();
+
+if($_SESSION['id']) {
+    header('Location: /main.php');
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title>templates</title>
+		<link href="style.css" rel="stylesheet">
+		<link href="login.css" rel="stylesheet">
+	</head>
+	<body>
+		<div id='app'>
+			<header class="cf">
+				<div id="header_wrap">
+					<div class="make_hcenter">
+						<div id="top_nav">
+						</div>
+					</div>
+				</div>
+			</header>
+			<div id="wrap" class="clearfix">
+				<div id="wrap_main">
+					<div id="container_main" class="container clearfix">
+						<div id="main_login" class="large">
+							<ul class="main_field">
+								<li>
+									<h1>Login</h1>
+								</li>
+							</ul>
+							<form action="login.php" id="login" method="post">
+								<ul id="login-password-fields" class="main_field">
+									<li>
+										<input class="form-text large" id="user_password"  placeholder="Password" maxlength="20" name="user[password]" size="20" type="password">
+									</li>
+								</ul>
+								<div class="main_button">
+									<input id="secret" name="secret" type="hidden">
+									<input id="save_button_login" name="commit" type="submit" value="Login">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<footer>
+				<div id="footer_wrap">
+					<div class="top_row">
+						<div class="links_container">
+							<div class="small_links">
+								&copy; Copyright by <a href='https://www.facebook.com/huangtao.me'>Tao Huang</a>, <a href='https://www.facebook.com/zilinj'>Zilin Jiang</a>, 2011
+							</div>
+						</div>
+						<div class="feedback_link_container">
+							<a href="http://www.cmu.edu/">Carnegie Mellon University</a>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
+

1ba2381bb5cde16a42efb804fab6e3d6af1759c7
Zilin Jiang <libragold@gmail.com>
2011-11-06 13:09:05 -0500

    fix

--- js/script.js
+++ js/script.js
@@ -80,8 +80,10 @@ var Votes = {
 	},
 	"refreshChart" : function() {
 		var V = this;
+		var n = Param.judges;
 		var scores = V.cache[V.currentTeam];
-		var total = scores[Param.judges+1] * 100;
+		if (not scores) for (var i = 0; i < n + 2; i++) scores[i] = 0;
+		var total = scores[n+1] * 100;
 		$('div.agenda-item', V.$chart).each(function(i) {
 			$(this).animate({ width : Math.max(scores[i] * 40 - 7, 33) });
 		});

a3823c2a606ccd48aa1dc50ab06656f65d4ecf1b
Zilin Jiang <libragold@gmail.com>
2011-11-17 18:17:44 -0500

    ui 60%

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var Param = {
-	"teams" : 5,
+	"teams" : 40,
 	"judges" : 3,
 	"ratio": 0.8
 };
@@ -26,15 +26,15 @@ var Votes = {
             $s = $li.clone().data('team', rank[i]).appendTo(V.$list)
                 .find('label').html('TEAM ' + rank[i]).end()
                 .find('span.rank-score').html((V.cache[rank[i]][Param.judges + 1] * 10).toFixed(1));
-			$s = s + "<li data-team='"+rank[i]+"'><span>• TEAM "+rank[i]+"</span><span class='rank-score'>"+(V.cache[rank[i]][Param.judges+1]*10).toFixed(1)+"</span></li>"
+//			$s = s + "<li data-team='"+rank[i]+"'><span>• TEAM "+rank[i]+"</span><span class='rank-score'>"+(V.cache[rank[i]][Param.judges+1]*10).toFixed(1)+"</span></li>"
         }
         for (var team=1; team<=Param.teams; team++)
         	if (! (team in V.cache))
                 $s = $li.clone().data('team', team).addClass('hidden').appendTo(V.$list)
                     .find('label').html('TEAM ' + team).end()
                     .find('span.rank-score').html('0.0');
-        		s = s + "<li style='display:none' data-team='"+team+"'><span>• TEAM "+team+"</span><span class='rank-score'>0.0</span></li>"
-      	V.$list.html(s);		
+//        		s = s + "<li style='display:none' data-team='"+team+"'><span>• TEAM "+team+"</span><span class='rank-score'>0.0</span></li>"
+//      V.$list.html(s);
 		return true;
     },
 	"fetch" : function() {

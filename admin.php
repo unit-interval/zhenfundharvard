7985a45cbdc5fa5d51a31796c588f2d19690b853
Tao <tao@huangtao.me>
2011-11-19 01:48:04 +0800

    admin page add console tab

--- admin.php
+++ admin.php
@@ -21,28 +21,35 @@ if($_POST['passphrase'] == ADMIN_PW) {
         </form>
 		<hr />
 <?php
-if($_POST['passphrase'] == 'gew') {
-    if($_POST['do'] == 'reset') {
+if($_POST['do'] == 'reset') {
+    if($admin_once) {
         $db->query("truncate `votes`");
         echo "<p>successifully trancated table 'votes'.</p>";
-    } elseif($_POST['do'] == 'votes') {
-        echo "<table><thead><tr><td>judge</td><td>team</td><td>score</td></tr></thead><tbody>";
-        $query = "select * from `votes` order by `judge_id`";
-        $result = $db->query($query);
-        while($row = $result->fetch_assoc())
-            echo "<tr><td>{$row['judge_id']}</td><td>{$row['team_id']}</td><td>{$row['score']}</td></tr>";
-        $result->free();
-        echo "</tbody></table>";
-
-    } elseif($_POST['do'] == 'show') {
-        echo "<table><thead><tr><td>#</td><td>passwd</td></tr></thead><tbody>";
-        $query = "select * from `judges` order by `id`";
-        $result = $db->query($query);
-        while($row = $result->fetch_assoc())
-            echo "<tr><td>{$row['id']}</td><td>{$row['passwd']}</td></tr>";
-        $result->free();
-        echo "</tbody></table>";
-    }
+    } else
+        echo "<p>passphrase needed for this operation.</p>";
+} elseif($_POST['do'] == 'votes') {
+    echo "<table><thead><tr><td>judge</td><td>team</td><td>score</td></tr></thead><tbody>";
+    $query = "select * from `votes` order by `judge_id`";
+    $result = $db->query($query);
+    while($row = $result->fetch_assoc())
+        echo "<tr><td>{$row['judge_id']}</td><td>{$row['team_id']}</td><td>{$row['score']}</td></tr>";
+    $result->free();
+    echo "</tbody></table>";
+} elseif($_POST['do'] == 'show') {
+    echo "<table><thead><tr><td>#</td><td>passwd</td></tr></thead><tbody>";
+    $query = "select * from `judges` order by `id`";
+    $result = $db->query($query);
+    while($row = $result->fetch_assoc())
+        echo "<tr><td>{$row['id']}</td><td>{$row['passwd']}</td></tr>";
+    $result->free();
+    echo "</tbody></table>";
+} elseif($_POST['do'] == 'console') {
+    $html = "<form id='matrix'><table><thead><tr>";
+    for($i = 1; $i <= NUM_TEAM; $i++)
+        $html .= "<td>Team $i</td>";
+    $html .= "</tr></thead><tbody";
+    //"</table>";
+    echo "$html";
 }
 ?>
     </body>
